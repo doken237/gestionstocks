88add3ce09aaec22a2636977e165b45ef06cd0d8
doken237 <dokfongang34@gmail.com>
2024-06-10 09:07:29 +0100

    modiff_bon

--- app/Http/Controllers/API/BonCommandeController.php
+++ app/Http/Controllers/API/BonCommandeController.php
@@ -37,5 +37,47 @@ class BonCommandeController extends Controller
         }
 }
 
-
+public function update(Request $request){
+    try{
+        $data=$request->all();
+        $article_id=$request['article_id'];
+        $user_id=Auth::user()->id;
+        $id=$request['id'];
+        $user=Bon_commande::where('id',$request['id'])->first(['id','user_id']);
+        $bons=Bon_commande::find($id);
+        if (!$bons){
+            return response()->json([
+                'message'=>'Ce bon n\'existe pas!',
+                'status'=>false,
+                'user'=>$bons->id
+            ]);
+            die();
+        }
+        if (($user->user_id!=$user_id)) {
+            return response()->json([
+                'message'=>'Ce  n\'est pas vous qui avez creer ce bon de commande!',
+                'status'=>false,
+                
+           
+                
+            ]);
+            die();
+        }
+        
+        $bons->update($data);
+        return response()->json([
+            'message'=>'Bon mis a jour Update',
+            'status'=>true,
+            'user'=>$user_id
+        ]);
+    }
+    catch(\Throwable $th)
+    {
+        return response()->json(
+            ['message'=>$th->getMessage(),
+            'status'=>false,
+            'user'=>$user->user_id
+            ]);       
+    }
+}
 }
